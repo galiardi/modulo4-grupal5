93d027fbc8ed2147939a02b4aedbebb8086c731d
galiardi <66950896+galiardi@users.noreply.github.com>
2023-06-04 13:19:22 -0400

    Add lazy loading to meal-card's images

--- js/ui/getMealCard.js
+++ js/ui/getMealCard.js
@@ -1,7 +1,7 @@
 function getMealCard({ idMeal, strMeal, strMealThumb }) {
   return `
     <div class="card my-3" style="width: 18rem;">
-      <img src="${strMealThumb}" class="card-img-top" alt="${strMeal}">
+      <img loadding="lazy" src="${strMealThumb}" class="card-img-top" alt="${strMeal}">
       <div class="card-body">
         <h5 class="card-title">${strMeal}</h5>
         <p class="card-text"></p>
